45226d1b2cd5e24478a72e661f9d8dc7041f6283
duranarf96@gmail.com <duranarf96@gmail.com>
2017-08-11 15:01:47 +0300

    kodlar degistı
    kodlar degstirilerek kontrol edildi

--- Sun_Tracking_System.ino
+++ Sun_Tracking_System.ino
@@ -1,39 +1,48 @@
-#include <Servo.h>
+#include <Servo.h> // include Servo library
+  
+Servo servo2; // horizontal servo
 
-Servo servo1;
-Servo servo2;
-int ldrlt = 0; //LDR sol üst
-int ldrrt = 1; //LDR sağ üst
-int ldrld = 2; //LDR sol alt
-int ldrrd = 3;
+  
+Servo servo1; // vertical servo
 
-
-
-
-void setup() 
+// LDR pin connections
+// name = analogpin;
+int ldrlt = A0; //LDR top left
+int ldrrt = A1; //LDR top rigt
+int ldrld = A2; //LDR down left
+int ldrrd = A3; //ldr down rigt
+  
+void setup()
 {
-  servo1.attach(9);
-  servo1.writeMicroseconds(1475);
-  servo2.attach(10);
-  servo2.writeMicroseconds(1475);
-
 Serial.begin(9600);
+// servo connections
+// name.attacht(pin);
+servo2.attach(9);
+servo1.attach(10);
 
-
-
+  servo1.writeMicroseconds(1475);
+  
+  servo2.writeMicroseconds(1475);
 }
-
-void loop() 
+  
+void loop()
 {
-  int lt = analogRead(ldrlt); // sol üst
-int rt = analogRead(ldrrt); // sağ üst
-int ld = analogRead(ldrld); // sol alt
-int rd = analogRead(ldrrd); // sağ alt
-int avt = (lt + rt) ; // average value top
-int avd = (ld + rd) ; // average value down
-int dn1=  (lt + ld);
-int dn2=  (rt + rd);
- 
+int lt = analogRead(ldrlt); // top left
+int rt = analogRead(ldrrt); // top right
+int ld = analogRead(ldrld); // down left
+int rd = analogRead(ldrrd); // down rigt
+
+int tol = 1;
+  
+int avt = (lt + rt)*10 ; // average value top
+int avd = (ld + rd)*10 ; // average value down
+int avl = (lt + ld)*10 ; // average value left
+int avr = (rt + rd)*10 ; // average value right
+  
+int ver = avt - avd; // check the diffirence of up and down
+int hor = avl - avr;// check the diffirence og left and rigt
+int dvert=abs(ver);
+int dhoriz=abs(hor);
  Serial.print("1. ");
 Serial.println(avt);
 delay(1000);
@@ -41,62 +50,56 @@ Serial.print("2. ");
 Serial.println(avd);
 delay(1000);
  Serial.print("3. ");
-Serial.println(dn1);
+Serial.println(avl);
 delay(1000);
  Serial.print("4. ");
-Serial.println(dn2);
+Serial.println(avr);
 delay(1000);
+  Serial.print("ver. ");
+Serial.println(dvert);
+delay(1000);
+ Serial.print("hor ");
+Serial.println(dhoriz);
+delay(1000);
+  
+if ( dvert > tol)
+{
+if (avt < avd)
+{
 
-  if (avt<avd)
-  {
-    
       servo1.writeMicroseconds(1515);
       delay(42);
       servo1.writeMicroseconds(1475);
       delay(200);
-      
-    
-  }
-  else if (avt>avd)
-  { 
-    
+}
+else if (avt > avd)
+{
+
      servo1 .writeMicroseconds(1425);
      delay(24);
      servo1.writeMicroseconds(1475);
-     delay(100);     
-   
-   }
-  else
-  {
-    delay(24);
-    servo1.writeMicroseconds(1475);
-    delay(24);
-    }
+     delay(100);
+}
 
-  if (dn1<dn2)
-  {
-    
+}
+if (  dhoriz > tol)
+{
+if (avl >avr)
+{
       servo2.writeMicroseconds(1515);
       delay(42);
       servo2.writeMicroseconds(1475);
-      delay(100);  
-    
-  }
-  else if (dn2<dn1)
-  { 
-    
-     servo2.writeMicroseconds(1425);
+      delay(100); 
+}
+else if (avl < avr)
+{
+servo2.writeMicroseconds(1425);
      delay(24);
      servo2.writeMicroseconds(1475);
      delay(100);
-   }
-  
-  else
-  {
-    delay(24);
-    servo2.writeMicroseconds(1475);
-    delay(24);
-    }
 
-  
 }
+}
+}
+
+
